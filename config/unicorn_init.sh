8300111b549971e70d9457919f0267163a843778
Owen Connor <owenconnor@gmail.com>
2014-09-26 18:48:11 +0100

    Updating unicorn config

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/personal-site/current
+APP_ROOT=/home/username/apps/personal-site/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
@@ -82,4 +82,3 @@ reopen-logs)
   exit 1
   ;;
 esac
-
