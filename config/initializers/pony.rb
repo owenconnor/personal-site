656573c65227bea11a098803d3414bd4862042df
Owen Connor <owenconnor@gmail.com>
2014-09-29 12:43:12 +0100

    Updated email and form

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -1,12 +1,12 @@
 Pony.options = {
-    :to => 'owenconnor@gmail.com',
+    :to => 'owen@owenconnor.net',
     :via => :smtp,
     :via_options => {
         :address => 'smtp.gmail.com',
         :port => '587',
         :enable_starttls_auto => true,
-        :user_name => 'owenconnor',
-        :password => 'qkfdtktsnkrkelen',
+        :user_name => 'owen@owenconnor.net',
+        :password => 'Ve850#zPz2I^',
         :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
         :domain => "localhost.localdomain" # the HELO domain provided by the client to the server
     }
