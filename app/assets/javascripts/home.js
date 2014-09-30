f86b6097c7e839fd440612670e3ac94d936f363b
Owen Connor <owenconnor@gmail.com>
2014-09-30 14:54:34 +0100

    Updated mobile styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,28 +13,31 @@ $(document).ready(function() {
         event.preventDefault(); // Prevent link from following its href
     });
 
-    $(".skill-section").mouseenter( function (){
-        $(this).closest(".skill-section").css({"background-color": "#3f3f3f"});
-        $(this).closest(".skill-section").find(".icon").hide();
-        $(this).closest(".skill-section").find(".skill-name").hide();
-        $(this).closest(".skill-section").find(".skill-text").hide();
-        $(this).closest(".skill-section").find(".skill-text").fadeIn('fast');
-        $(this).closest(".skill-section").find(".skill-text-extended").fadeIn('fast');
-        $(this).closest(".skill-section").find(".skill-name").css({"color": "#ffffff"});
-        $(this).closest(".skill-section").find(".skill-name").fadeIn('fast');
-    });
+    if (screen.width >= 800) {
+        $(".skill-section").mouseenter( function (){
+            $(this).closest(".skill-section").css({"background-color": "#3f3f3f"});
+            $(this).closest(".skill-section").find(".icon").hide();
+            $(this).closest(".skill-section").find(".skill-name").hide();
+            $(this).closest(".skill-section").find(".skill-text").hide();
+            $(this).closest(".skill-section").find(".skill-text").fadeIn('fast');
+            $(this).closest(".skill-section").find(".skill-text-extended").fadeIn('fast');
+            $(this).closest(".skill-section").find(".skill-name").css({"color": "#ffffff"});
+            $(this).closest(".skill-section").find(".skill-name").fadeIn('fast');
+        });
 
-    $(".skill-section").mouseleave( function (){
-        $(this).closest(".skill-section").css({"background-color": "inherit"});
-        $(this).closest(".skill-section").find(".skill-text-extended").hide();
-        $(this).closest(".skill-section").find(".skill-text").hide();
-        $(this).closest(".skill-section").find(".skill-name").hide();
-        $(this).closest(".skill-section").find(".skill-text").fadeIn(400);
-        $(this).closest(".skill-section").find(".icon").fadeIn(400);
-        $(this).closest(".skill-section").find(".skill-name").css({"color": "#2E2F30"});
-        $(this).closest(".skill-section").find(".skill-name").fadeIn(400);
+        $(".skill-section").mouseleave( function (){
+            $(this).closest(".skill-section").css({"background-color": "inherit"});
+            $(this).closest(".skill-section").find(".skill-text-extended").hide();
+            $(this).closest(".skill-section").find(".skill-text").hide();
+            $(this).closest(".skill-section").find(".skill-name").hide();
+            $(this).closest(".skill-section").find(".skill-text").fadeIn(400);
+            $(this).closest(".skill-section").find(".icon").fadeIn(400);
+            $(this).closest(".skill-section").find(".skill-name").css({"color": "#2E2F30"});
+            $(this).closest(".skill-section").find(".skill-name").fadeIn(400);
 
-    });
+        });
+    }
+
 
     $(document).ready(function () {
         $("#new_contact").validate({
