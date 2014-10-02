04439f0f7321d155e02ca1833dae32e3e9375895
Owen Connor <owenconnor@gmail.com>
2014-10-02 13:29:52 +0100

    adding unicorn for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,23 @@
-root = "/home/deployer/apps/personal-site_production/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+# config/unicorn.rb
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
-listen "/var/sockets/unicorn.personal-site_production.sock"
-worker_processes 2
-timeout 30
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
 
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
 end
